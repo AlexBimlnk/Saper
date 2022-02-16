a61df06ac7ce44263a3cc64e98ed671536614fd9
AlexBim <centuriin@yandex.ru>
2022-02-16 15:51:02 +0300

    Init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.saper {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.saper to javafx.fxml;
+    exports com.example.saper;
+}
