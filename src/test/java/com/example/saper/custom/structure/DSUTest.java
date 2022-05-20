dfe48db2fd39bf3b33fd95a3d824978c296919cf
centuriin <centuriin@yandex.ru>
2022-05-20 20:28:24 +0300

    Tests refactoring

--- src/test/java/com/example/saper/custom/structure/DSUTest.java
+++ src/test/java/com/example/saper/custom/structure/DSUTest.java
@@ -1,15 +1,15 @@
 package com.example.saper.custom.structure;
 
 import javafx.util.Pair;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.*;
 
 import java.security.InvalidParameterException;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 class DSUTest {
 
@@ -17,221 +17,259 @@ class DSUTest {
     private DSU _dsu;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         _dsu = new DSU(_size);
     }
-
     @AfterEach
-    void tearDown() {
+    public void tearDown() {
         _dsu = null;
     }
 
-    @Test
-    void makeSet() {
-        //arrange
+
+    @TestFactory
+    public Collection<DynamicTest> can_Make_Set() {
+        // Arrange
         var simplePair = new Pair<>(1, 1);
-        int firstIndexElement = 1;
-        int secondIndexElement = -1;
+        int validIndex = 1;
+        int outOfBoundsIndex = -1;
 
-        //act
-        var indexOutOfBoundsException = assertThrows(IndexOutOfBoundsException.class,
-                () -> _dsu.MakeSet(secondIndexElement, simplePair));
-
-        //assert
-        assertNotNull(indexOutOfBoundsException.getMessage());
-        assertDoesNotThrow(() -> _dsu.MakeSet(firstIndexElement, simplePair));
+        // Assert
+        return Arrays.asList(
+                dynamicTest("Throws IndexOutOfBoundsException when index is invalid",
+                        () -> assertThrows(
+                                IndexOutOfBoundsException.class,
+                                () -> _dsu.MakeSet(outOfBoundsIndex, simplePair))),
+                dynamicTest("Can make set",
+                        () -> assertDoesNotThrow(
+                                () -> _dsu.MakeSet(validIndex, simplePair)))
+        );
     }
 
-    @Test
-    void findSet() {
-        //arrange
-        int firstIndexElement = 1;
-        int secondIndexElement = 2;
-        int thirdIndexElement = -3;
+
+    @TestFactory
+    public Collection<DynamicTest> can_Find_Set() {
+        // Arrange
         var simplePair = new Pair<>(1, 1);
-        _dsu.MakeSet(firstIndexElement, simplePair);
+        int validIndex = 1;
+        int expectedValue = 1;
+        int outOfBoundsIndex = -1;
+        int invalidParameterIndex = 2;
 
-        //act
-        var indexOutOfBoundsException = assertThrows(IndexOutOfBoundsException.class,
-                () -> _dsu.FindSet(thirdIndexElement));
-        var invalidParameterException = assertThrows(InvalidParameterException.class,
-                () -> _dsu.FindSet(secondIndexElement));
+        // Act
+        _dsu.MakeSet(validIndex, simplePair);
 
-
-        //assert
-        assertEquals(firstIndexElement, _dsu.FindSet(firstIndexElement));
-        assertNotNull(indexOutOfBoundsException.getMessage());
-        assertNotNull(invalidParameterException.getMessage());
+        // Assert
+        return Arrays.asList(
+                dynamicTest("Throws IndexOutOfBoundsException when index is invalid",
+                        () -> assertThrows(
+                                IndexOutOfBoundsException.class,
+                                () -> _dsu.FindSet(outOfBoundsIndex))),
+                dynamicTest("Throws InvalidParameterException when index is invalid",
+                        () -> assertThrows(
+                                InvalidParameterException.class,
+                                () -> _dsu.FindSet(invalidParameterIndex))),
+                dynamicTest("Can make set",
+                        () -> assertEquals(expectedValue, _dsu.FindSet(validIndex)))
+        );
     }
 
-    @Test
-    void unionSets() {
-        //arrange
+
+    @TestFactory
+    public Collection<DynamicTest> can_Union_Sets() {
+        // Arrange
         int[] indexElements = {0, 2, 3, 5, 1, 7, 8};
-        int notExistingSetIndexElement = 9;
-        int invalidIndexElement = -3;
         var simplePair = new Pair<>(1, 1);
+        int invalidParameterIndex = 9;
+        int outOfBoundsIndex = -3;
+
+        // Act
         for (int indexElement : indexElements) {
             _dsu.MakeSet(indexElement, simplePair);
         }
 
-        //act
-        var indexOutOfBoundsException = assertThrows(IndexOutOfBoundsException.class,
-                () -> _dsu.UnionSets(invalidIndexElement, indexElements[0]));
-        var invalidParameterException = assertThrows(InvalidParameterException.class,
-                () -> _dsu.UnionSets(notExistingSetIndexElement, indexElements[0]));
         _dsu.UnionSets(indexElements[0], indexElements[1]);
         _dsu.UnionSets(indexElements[5], indexElements[4]);
         _dsu.UnionSets(indexElements[0], indexElements[2]);
         _dsu.UnionSets(indexElements[1], indexElements[3]);
 
-        //assert
-        assertNotNull(indexOutOfBoundsException.getMessage());
-        assertNotNull(invalidParameterException.getMessage());
-
-        assertEquals(indexElements[0], _dsu.FindSet(indexElements[0]));
-        assertEquals(indexElements[0], _dsu.FindSet(indexElements[1]));
-        assertEquals(indexElements[0], _dsu.FindSet(indexElements[2]));
-        assertEquals(indexElements[0], _dsu.FindSet(indexElements[3]));
-        assertEquals(indexElements[5], _dsu.FindSet(indexElements[4]));
-        assertEquals(indexElements[5], _dsu.FindSet(indexElements[5]));
-        assertEquals(indexElements[6], _dsu.FindSet(indexElements[6]));
+        // Assert
+        return Arrays.asList(
+                dynamicTest("Throws IndexOutOfBoundsException when index is invalid",
+                        () -> assertThrows(
+                                IndexOutOfBoundsException.class,
+                                () -> _dsu.UnionSets(outOfBoundsIndex, 0))),
+                dynamicTest("Throws InvalidParameterException when index is invalid",
+                        () -> assertThrows(
+                                InvalidParameterException.class,
+                                () -> _dsu.UnionSets(invalidParameterIndex, 0))),
+                dynamicTest("1 union test",
+                        () -> assertEquals(0, _dsu.FindSet(0))),
+                dynamicTest("2 union test",
+                        () -> assertEquals(0, _dsu.FindSet(2))),
+                dynamicTest("3 union test",
+                        () -> assertEquals(0, _dsu.FindSet(3))),
+                dynamicTest("4 union test",
+                        () -> assertEquals(0, _dsu.FindSet(5))),
+                dynamicTest("5 union test",
+                        () -> assertEquals(7, _dsu.FindSet(1))),
+                dynamicTest("6 union test",
+                        () -> assertEquals(7, _dsu.FindSet(7))),
+                dynamicTest("7 union test",
+                        () -> assertEquals(8, _dsu.FindSet(8)))
+        );
     }
 
-    @Test
-    void getAllSetElem() {
-        //arrange
+
+    @TestFactory
+    public Collection<DynamicTest> can_Get_All_Set_Elements() {
+        // Arrange
         int[] indexElements = {0, 2, 3, 5, 1, 7, 8};
-        int notExistingSetIndexElement = 9;
-        int invalidIndexElement = -3;
         var simplePair = new Pair<>(1, 1);
+        int invalidParameterIndex = 9;
+        int outOfBoundsIndex = -3;
+        List<Integer> expectedListSet1 = Arrays.asList(0, 2, 3, 5);
+        List<Integer> expectedListSet2 = Arrays.asList(1, 7);
+        List<Integer> expectedListSet3 = Arrays.asList(8);
+
+        // Act
         for (int indexElement : indexElements) {
             _dsu.MakeSet(indexElement, simplePair);
         }
-        ArrayList<Integer> firstListSet = new ArrayList<>(4);
-        firstListSet.add(indexElements[0]);
-        firstListSet.add(indexElements[1]);
-        firstListSet.add(indexElements[2]);
-        firstListSet.add(indexElements[3]);
-        ArrayList<Integer> secondListSet = new ArrayList<>(2);
-        secondListSet.add(indexElements[4]);
-        secondListSet.add(indexElements[5]);
-        ArrayList<Integer> thirdListSet = new ArrayList<>(1);
-        thirdListSet.add(indexElements[6]);
 
-        //act
-        var indexOutOfBoundsException = assertThrows(IndexOutOfBoundsException.class,
-                () -> _dsu.GetAllSetElem(invalidIndexElement));
-        var invalidParameterException = assertThrows(InvalidParameterException.class,
-                () -> _dsu.GetAllSetElem(notExistingSetIndexElement));
         _dsu.UnionSets(indexElements[0], indexElements[1]);
         _dsu.UnionSets(indexElements[5], indexElements[4]);
         _dsu.UnionSets(indexElements[0], indexElements[2]);
         _dsu.UnionSets(indexElements[1], indexElements[3]);
 
-        //assert
-        assertNotNull(indexOutOfBoundsException.getMessage());
-        assertNotNull(invalidParameterException.getMessage());
-
-        assertEquals(firstListSet, _dsu.GetAllSetElem(indexElements[0]));
-        assertEquals(firstListSet, _dsu.GetAllSetElem(indexElements[1]));
-        assertEquals(firstListSet, _dsu.GetAllSetElem(indexElements[2]));
-        assertEquals(firstListSet, _dsu.GetAllSetElem(indexElements[3]));
-        assertEquals(secondListSet, _dsu.GetAllSetElem(indexElements[4]));
-        assertEquals(secondListSet, _dsu.GetAllSetElem(indexElements[5]));
-        assertEquals(thirdListSet, _dsu.GetAllSetElem(indexElements[6]));
+        // Assert
+        return Arrays.asList(
+                dynamicTest("Throws IndexOutOfBoundsException when index is invalid",
+                        () -> assertThrows(
+                                IndexOutOfBoundsException.class,
+                                () -> _dsu.GetAllSetElem(outOfBoundsIndex))),
+                dynamicTest("Throws InvalidParameterException when index is invalid",
+                        () -> assertThrows(
+                                InvalidParameterException.class,
+                                () -> _dsu.GetAllSetElem(invalidParameterIndex))),
+                dynamicTest("1 get all set elements", () -> {
+                    for (int i = 0; i <= 3; i++) {
+                        assertIterableEquals(expectedListSet1, _dsu.GetAllSetElem(i));
+                    }
+                }),
+                dynamicTest("2 get all set elements", () -> {
+                    assertIterableEquals(expectedListSet2, _dsu.GetAllSetElem(1));
+                    assertIterableEquals(expectedListSet2, _dsu.GetAllSetElem(7));
+                }),
+                dynamicTest("3 get all set elements", () -> assertIterableEquals(expectedListSet3, _dsu.GetAllSetElem(8)))
+        );
     }
 
+
     @Test
-    void getAllUniqueSets() {
-        //arrange
+    public void can_Get_All_Unique_Sets() {
+        // Arrange
         int[] indexElements = {0, 2, 3, 5, 1, 7, 8};
         var simplePair = new Pair<>(1, 1);
+        List<Integer> expectedList = Arrays.asList(0, 7, 8);
+
+        // Act
         for (int indexElement : indexElements) {
             _dsu.MakeSet(indexElement, simplePair);
         }
-        ArrayList<Integer> listLeadersOfSets = new ArrayList<>(4);
-        listLeadersOfSets.add(indexElements[0]);
 
-        listLeadersOfSets.add(indexElements[5]);
-
-        listLeadersOfSets.add(indexElements[6]);
-
-        //act
         _dsu.UnionSets(indexElements[0], indexElements[1]);
         _dsu.UnionSets(indexElements[5], indexElements[4]);
         _dsu.UnionSets(indexElements[0], indexElements[2]);
         _dsu.UnionSets(indexElements[1], indexElements[3]);
 
-        //assert
-        assertEquals(listLeadersOfSets, _dsu.GetAllUniqueSets());
+        // Assert
+        assertIterableEquals(expectedList, _dsu.GetAllUniqueSets());
     }
 
-    @Test
-    void getElemInSet() {
-        //arrange
+
+    @TestFactory
+    public Collection<DynamicTest> can_Get_Elements_In_Set() {
+        // Arrange
         int[] indexElements = {0, 2, 3, 5, 1, 7, 8};
-        int notExistingSetIndexElement = 9;
-        int invalidIndexElement = -3;
-        for (int i = 0; i<indexElements.length; i++) {
+        int invalidParameterIndex = 9;
+        int outOfBoundsIndex = -3;
+
+        // Act
+        for (int i = 0; i < indexElements.length; i++) {
             _dsu.MakeSet(indexElements[i], new Pair<>(i+1, i+1));
         }
 
-        //act
-        var indexOutOfBoundsException = assertThrows(IndexOutOfBoundsException.class,
-                () -> _dsu.getElemInSet(invalidIndexElement));
-        var invalidParameterException = assertThrows(InvalidParameterException.class,
-                () -> _dsu.getElemInSet(notExistingSetIndexElement));
         _dsu.UnionSets(indexElements[0], indexElements[1]);
         _dsu.UnionSets(indexElements[5], indexElements[4]);
         _dsu.UnionSets(indexElements[0], indexElements[2]);
         _dsu.UnionSets(indexElements[1], indexElements[3]);
 
-        //assert
-        assertNotNull(indexOutOfBoundsException.getMessage());
-        assertNotNull(invalidParameterException.getMessage());
-
-        for (int i = 0; i<indexElements.length; i++) {
-            assertEquals(new Pair<>(i+1, i+1), _dsu.getElemInSet(indexElements[i]));
-        }
+        // Assert
+        return Arrays.asList(
+                dynamicTest("Throws IndexOutOfBoundsException when index is invalid",
+                        () -> assertThrows(
+                                IndexOutOfBoundsException.class,
+                                () -> _dsu.getElemInSet(outOfBoundsIndex))),
+                dynamicTest("Throws InvalidParameterException when index is invalid",
+                        () -> assertThrows(
+                                InvalidParameterException.class,
+                                () -> _dsu.getElemInSet(invalidParameterIndex))),
+                dynamicTest("Can get elements", () -> {
+                    for (int i = 0; i<indexElements.length; i++) {
+                        assertEquals(new Pair<>(i+1, i+1), _dsu.getElemInSet(indexElements[i]));
+                    }
+                })
+        );
     }
 
-    @Test
-    void getSetSize() {
-        //arrange
+
+    @TestFactory
+    public Collection<DynamicTest> can_Get_Size() {
+        // Arrange
         int[] indexElements = {0, 2, 3, 5, 1, 7, 8};
-        int notExistingSetIndexElement = 9;
-        int invalidIndexElement = -3;
+        int invalidParameterIndex = 9;
+        int outOfBoundsIndex = -3;
         var simplePair = new Pair<>(1, 1);
+
+        // Act
         for (int indexElement : indexElements) {
             _dsu.MakeSet(indexElement, simplePair);
         }
 
-        //act
-        var indexOutOfBoundsException = assertThrows(IndexOutOfBoundsException.class,
-                () -> _dsu.GetAllSetElem(invalidIndexElement));
-        var invalidParameterException = assertThrows(InvalidParameterException.class,
-                () -> _dsu.GetAllSetElem(notExistingSetIndexElement));
         _dsu.UnionSets(indexElements[0], indexElements[1]);
         _dsu.UnionSets(indexElements[5], indexElements[4]);
         _dsu.UnionSets(indexElements[0], indexElements[2]);
         _dsu.UnionSets(indexElements[1], indexElements[3]);
 
-        //assert
-        assertNotNull(indexOutOfBoundsException.getMessage());
-        assertNotNull(invalidParameterException.getMessage());
-
-        assertEquals(4, _dsu.getSetSize(indexElements[0]));
-        assertEquals(1, _dsu.getSetSize(indexElements[1]));
-        assertEquals(1, _dsu.getSetSize(indexElements[2]));
-        assertEquals(1, _dsu.getSetSize(indexElements[3]));
-        assertEquals(1, _dsu.getSetSize(indexElements[4]));
-        assertEquals(2, _dsu.getSetSize(indexElements[5]));
-        assertEquals(1, _dsu.getSetSize(indexElements[6]));
+        // Assert
+        return Arrays.asList(
+                dynamicTest("Throws IndexOutOfBoundsException when index is invalid",
+                        () -> assertThrows(
+                                IndexOutOfBoundsException.class,
+                                () -> _dsu.getSetSize(outOfBoundsIndex))),
+                dynamicTest("Throws InvalidParameterException when index is invalid",
+                        () -> assertThrows(
+                                InvalidParameterException.class,
+                                () -> _dsu.getSetSize(invalidParameterIndex))),
+                dynamicTest("1 union test",
+                        () -> assertEquals(4, _dsu.getSetSize(0))),
+                dynamicTest("2 union test",
+                        () -> assertEquals(1, _dsu.getSetSize(2))),
+                dynamicTest("3 union test",
+                        () -> assertEquals(1, _dsu.getSetSize(3))),
+                dynamicTest("4 union test",
+                        () -> assertEquals(1, _dsu.getSetSize(5))),
+                dynamicTest("5 union test",
+                        () -> assertEquals(1, _dsu.getSetSize(1))),
+                dynamicTest("6 union test",
+                        () -> assertEquals(2, _dsu.getSetSize(7))),
+                dynamicTest("7 union test",
+                        () -> assertEquals(1, _dsu.getSetSize(8)))
+        );
     }
 
     @Test
-    void getSetsSpace() {
+    void can_Get_Sets_Space() {
         assertEquals(_size, _dsu.getSetsSpace());
     }
 }
